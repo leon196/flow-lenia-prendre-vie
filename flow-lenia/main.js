360211c48f92554fb410d29e561f646ac57787bb
Leon <lowpolygonum@gmail.com>
2025-02-03 13:29:18 +0100

    clean

--- flow-lenia/main.js
+++ flow-lenia/main.js
@@ -30,15 +30,13 @@ let body=html`
 addElm(body,document.body);
 body.disolve();
 
-let display=new CanvasDisplay(canvasElm);
-display.view=new Cam(Vec(0,0),1.);
-let control=new Control();
+let display = new CanvasDisplay(canvasElm);
+display.view = new Cam(Vec(0,0),1.);
+let control = new Control();
 control.connect(canvasElm);
 
-let shaderManager=new ShaderManager();
-let lenia=new Lenia();
-// let leniaLayer2=new Lenia();
-// let leniaLayer3=new Lenia();
+let shaderManager = new ShaderManager();
+let lenia = new Lenia();
 let leniaLayers = [lenia];
 
 let canvasTex=new Texture({
@@ -49,42 +47,15 @@ let canvasTex=new Texture({
 
 let composeShader = new ComposeShader();
 
-let imageTex=new Texture({
+let imageTex = new Texture({
 	src: imageSrc
 });
-let time=0;
+let time = 0;
 
 // play/pause with key space
-let update=true;
+let update = true;
 const key_space = 32;
 control.callbacks[key_space] = () => { update = !update; }
-
-// label
-const dom_label = document.createElement("div");
-dom_label.classList.add("label");
-document.body.append(dom_label);
-dom_label.textContent = "";
-
-// print label content
-control.callbacks['clic'] = () => { console.log(dom_label.textContent); }
-
-settings.updateMultiLayers = () => {
-	if (settings.multiLayers) {
-		leniaLayers = [lenia, leniaLayer2];
-	} else {
-		leniaLayers = [lenia];
-	}
-}
-
-// settings
-settings.imageHD = () => {
-	const img = new Image();
-	img.src = "./img/prendre-vie"+(settings.image4K?"":"-sd")+".png";
-	img.onload = () => {
-		imageTex = new Texture({ src: img });
-		leniaLayers.forEach(layer => layer.resize(img));
-	}
-};
 
 // reset
 settings.reset = () => {
@@ -94,8 +65,6 @@ settings.reset = () => {
 // zoom
 let zoom = 1.;
 let offset = [0,0];
-// let zoom2 = 1.;
-// let offset2 = [0,0];
 
 settings.onZoom = () => {
 	if (anim.current == undefined) {
@@ -109,45 +78,13 @@ if (settings.zoom) anim.start();
 
 // gui
 var gui = new dat.GUI();
-var folderSpeed = gui.addFolder('Speed');
-folderSpeed.add(settings, 'velocitySpeed', 0, 1, 0.01);
-folderSpeed.add(settings, 'gradientSpeed', 0, 1, 0.01);
-folderSpeed.open();
-var folderColor = gui.addFolder('Color');
-folderColor.add(settings, 'colorDNA', 0, 1, 0.01);
-folderColor.add(settings, 'colorVariation', 0, 1, 0.01);
-folderColor.open();
-var folderSpawn = gui.addFolder('Spawn');
-folderSpawn.add(settings, 'spawnEdge');
-folderSpawn.open();
-var folderImage = gui.addFolder('Image');
-folderImage.add(settings, 'blendImageInGradient');
-folderImage.add(settings, 'blendImageInLenia');
-folderImage.add(settings, 'multiLayers').onChange(settings.updateMultiLayers);
-folderImage.add(settings, 'image4K').onChange(settings.imageHD);
-folderImage.add(settings, 'zoom').onChange(settings.onZoom); 
-// folderImage.add(settings, 'geneUpdate').onChange(() => {
-// 	var seed = Math.floor(Math.random()*1000000);
-// 	lenia.geneUpdate(seed);
-// 	console.log("gene:"+seed)
-// }); 
-folderImage.add(settings, 'dnaUpdate').onChange(() => {
-	var seed = Math.floor(Math.random()*1000000);
-	lenia.dnaUpdate(seed);
-	console.log("dna:"+seed)
-}); 
-folderImage.open();
+gui.add(settings, 'velocitySpeed', 0, 1, 0.01);
+gui.add(settings, 'gradientSpeed', 0, 1, 0.01);
+gui.add(settings, 'zoom').onChange(settings.onZoom);
 gui.add(settings, 'reset'); 
-gui.remember(settings);
 gui.close();
 
 lenia.geneUpdate(315969);
-
-// setInterval(()=>{
-// 	var seed = Math.floor(Math.random()*1000000);
-// 	lenia.geneUpdate(seed);
-// 	console.log("gene:"+seed)
-// }, 15 * 1000)
 
 let elapsed = 0;
 
@@ -169,27 +106,5 @@ let frameAnim=animate((timeElapsed)=>{
 	leniaLayers.forEach(layer => layer.run(update,display,canvasTex,imageTex));
 
 	composeShader.run(display.view, lenia.size, display.size, zoom, offset, leniaLayers, settings);
-
-	// pick a specie
-	// if (control.mLDown)
-	// {
-	// 	const l = lenia;
-
-	// 	// label position
-	// 	const mouse = control.mousePos();
-		
-	// 	// read back pixel dna
-	// 	const read = new Float32Array(4);
-	// 	const x = mouse[0]*l.size[0]/canvasElm.width;
-	// 	const y = mouse[1]*l.size[1]/canvasElm.height;
-
-	// 	l.dnaTexPP.readAt(x,y,gl.RGBA, gl.FLOAT,read);
-
-	// 	// update label
-	// 	dom_label.textContent = read[0] + ',' + read[1] + ',' + read[2] + ',' + read[3];
-
-	// 	l.dnaSelect = read;
-
-	// }
 
 },1,true).start();
--- flow-lenia/settings.js
+++ flow-lenia/settings.js
@@ -2,7 +2,7 @@
 // MODIFY THESE VARIABLES AS NEEDED
 //----------------------------------
 
-let imageSrc="./img/blur-z1.png";
+let imageSrc="./img/prendre-vie-2k.png";
 let imageScale=1.;
 
 //creates a border on the image of random mutations, set to 0 to turn off
